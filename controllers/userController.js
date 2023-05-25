7334dd6a1c56ba36544fbaccdf8bff7de873571f
nedys55 <denysyuksek@gmail.com>
2023-05-25 19:18:46 +0200

    Add Bcrypt

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,15 +1,20 @@
-const express = require('express');
-const User = require('../models/userModel');
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const User = require("../models/userModel");
+
 const register = async (req, res) => {
-    const {name, email, password} = req.body;
-    try {
-        const user = await User.create({name, email, password});
-        res.status(201).send(user);
-    } catch (error) {
-        res.status(400).send(error);
-    }
-}
+  const { name, email, password } = req.body;
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({ name, email, password: hashedPassword });
+
+    res.status(201).send(user);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
 
 module.exports = {
-    register
-}
+  register
+};
